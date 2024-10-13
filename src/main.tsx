ad3d504094b906704d99c6250dd090543c263611
Miguel Ortiz <ortizmichel390@gmail.com>
2024-10-13 02:49:58 -0300

    framer motion was added to AboutMe

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { NextUIProvider } from '@nextui-org/react'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { NextUIProvider } from "@nextui-org/react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <NextUIProvider>
-    <App />
+      <App />
     </NextUIProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
