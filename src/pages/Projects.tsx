87b735600342666b0ae152befe643c90f46795e6
Miguel Ortiz <ortizmichel390@gmail.com>
2024-11-19 23:48:39 -0300

    New Refactions

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,7 +3,12 @@ import { useState, useEffect } from "react";
 import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
 import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 
-const projects = [
+interface Project {
+  title: string;
+  description: string;
+}
+
+const projects: Project[] = [
   {
     title: "Music World",
     description: "Proyecto 1: Music World...",
@@ -19,9 +24,9 @@ const projects = [
 ];
 
 export default function Projects() {
-  const [index, setIndex] = useState(0);
-  const [direction, setDirection] = useState(0);
-  const [isMounted, setIsMounted] = useState(false);
+  const [index, setIndex] = useState<number>(0);
+  const [direction, setDirection] = useState<number>(0);
+  const [isMounted, setIsMounted] = useState<boolean>(false);
 
   const variants = {
     enter: (direction: number) => ({
@@ -61,9 +66,7 @@ export default function Projects() {
 
   const navigatePrev = () => {
     setDirection(-1);
-    setIndex(
-      (prevIndex) => (prevIndex - 1 + projects.length) % projects.length
-    );
+    setIndex((prevIndex) => (prevIndex - 1 + projects.length) % projects.length);
   };
 
   useEffect(() => {
@@ -71,37 +74,51 @@ export default function Projects() {
   }, []);
 
   return (
-    <div className="relative flex items-center justify-center h-screen">
-      {/* Flecha Izquierda */}
-      <motion.button
-        onClick={navigatePrev}
-        className="absolute left-20 top-[calc(120px)] transform -translate-y-1/2 text-white bg-[#F50057] rounded-full w-12 h-8 flex items-center justify-center"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 2 }}
-      >
-        <KeyboardDoubleArrowLeftIcon />
-      </motion.button>
+    <div className="relative flex items-center justify-center h-screen overflow-hidden px-4">
+      {/* Flechas e indicadores */}
+      <div className="absolute top-28 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-5xl px-4 md:px-8 lg:px-16 flex items-center justify-between">
+        {/* Flecha Izquierda */}
+        <motion.button
+          onClick={navigatePrev}
+          className="hidden md:flex absolute left-4 -mt-5 text-white bg-[#F50057] rounded-full w-12 h-8 items-center justify-center z-10"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+        >
+          <KeyboardDoubleArrowLeftIcon />
+        </motion.button>
 
-      <div className="relative w-full max-w-5xl h-[90%] px-4 md:px-8 lg:px-16 mt-48">
-        {/* Menú superior que indica el proyecto actual */}
-        <div className="absolute -top-10 left-1/2 transform -translate-x-1/2 mt-4">
-          <div className="flex gap-4">
-            {projects.map((project, idx) => (
-              <motion.div
-                key={project.title}
-                className={`w-4 h-4 rounded-full border-2 ${
-                  idx === index ? "bg-white border-[#F50057]" : "bg-gray-400 border-gray-600"
-                }`}
-                title={project.title}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1, delay: idx * 0.2 }}
-              />
-            ))}
-          </div>
+        {/* Indicadores de proyectos */}
+        <div className="flex gap-4 mx-auto">
+          {projects.map((project, idx) => (
+            <motion.div
+              key={project.title}
+              className={`w-4 h-4 rounded-full border-2 ${
+                idx === index
+                  ? "bg-white border-[#F50057]"
+                  : "bg-gray-400 border-gray-600"
+              }`}
+              title={project.title}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: idx * 0.2 }}
+            />
+          ))}
         </div>
 
+        {/* Flecha Derecha */}
+        <motion.button
+          onClick={navigateNext}
+          className="hidden md:flex absolute right-4 -mt-5 text-white bg-[#F50057] rounded-full w-12 h-8 items-center justify-center z-10"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+        >
+          <KeyboardDoubleArrowRightIcon />
+        </motion.button>
+      </div>
+
+      <div className="relative w-full max-w-5xl h-[75%] px-4 md:px-8 lg:px-16 mt-20">
         <AnimatePresence initial={false} custom={direction}>
           <motion.div
             key={projects[index].title}
@@ -114,7 +131,7 @@ export default function Projects() {
             drag="x"
             dragConstraints={{ left: 0, right: 0 }}
             onDragEnd={handleDragEnd}
-            className="absolute -top-10 left-0 w-full h-full bg-white bg-opacity-20 backdrop-blur-md rounded-lg p-6 flex flex-col justify-center mt-16 border border-4 border-[#F50057]"
+            className="absolute -top-10 left-0 w-full h-full bg-white bg-opacity-20 backdrop-blur-md rounded-lg p-6 flex flex-col justify-center mt-16 border border-4 border-[#F50057] overflow-hidden"
           >
             {/* Contenido del proyecto */}
             <h2 className="text-white text-4xl font-bold mb-4">
@@ -136,17 +153,6 @@ export default function Projects() {
           </motion.div>
         </AnimatePresence>
       </div>
-
-      {/* Flecha Derecha */}
-      <motion.button
-        onClick={navigateNext}
-        className="absolute right-20 top-[calc(120px)] transform -translate-y-1/2 text-white bg-[#F50057] rounded-full w-12 h-8 flex items-center justify-center"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 2 }}
-      >
-        <KeyboardDoubleArrowRightIcon />
-      </motion.button>
     </div>
   );
 }
